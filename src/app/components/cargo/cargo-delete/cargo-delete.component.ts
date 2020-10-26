73c5ab977487a939bd234ba7c7b39b6a5852c17d
Lucas Briguenti <lucasbriguenti@outlook.com>
2020-10-26 08:41:07 -0300

    cargo delete

--- src/app/components/cargo/cargo-delete/cargo-delete.component.ts
+++ src/app/components/cargo/cargo-delete/cargo-delete.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { Router } from "@angular/router";
+import { CargoService } from "./../cargo.service";
+import { Cargo } from "./../cargo.model";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-cargo-delete',
-  templateUrl: './cargo-delete.component.html',
-  styleUrls: ['./cargo-delete.component.css']
+  selector: "app-cargo-delete",
+  templateUrl: "./cargo-delete.component.html",
+  styleUrls: ["./cargo-delete.component.css"],
 })
 export class CargoDeleteComponent implements OnInit {
-
-  constructor() { }
+  cargo: Cargo;
+  constructor(
+    private service: CargoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    var id = this.route.snapshot.paramMap.get("id");
+    this.service.readById(id).subscribe((cargo) => (this.cargo = cargo));
   }
 
+  createCargo(): void {
+    this.service.delete(this.cargo.id.toString()).subscribe(() => {
+      this.service.showMessage("Cargo deletado com sucesso!");
+      this.cancel();
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(["/cargos"]);
+  }
 }
